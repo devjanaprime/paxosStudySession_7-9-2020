a5df9b4991a74499c9376e19d663f004a071de19
Dev Jana <devjana@Devs-MBP-2.domain>
2020-07-09 08:37:23 -0500

    template component

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component{
+  
+  render() {
+    return (
+      <div>
+        <p>app</p>
+      </div>
+    ); // end jsx
+  } // end render
+} // end class
+
+export default App;
